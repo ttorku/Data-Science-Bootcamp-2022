0a2c00c7553cead5699ee5c994dab6990d01542a
Thomas Torku <43274079+ttorku@users.noreply.github.com>
2023-11-01 10:51:33 -0400

    Create extract.py

---
+++ NLP/extract.py
@@ -0,0 +1,39 @@
+import requests
+from io import BytesIO
+
+# Read the Excel file
+df_updated = pd.read_excel(excel_filename_new)
+
+# Function to download a PDF from a URL and extract the summary
+def extract_summary_from_url(url):
+    try:
+        # Send HTTP GET request to download the PDF
+        response = requests.get(url)
+        response.raise_for_status()
+        
+        # Open the PDF from the byte stream
+        with BytesIO(response.content) as file:
+            reader = PyPDF2.PdfReader(file)
+            text = ''
+            for page in reader.pages:
+                text += page.extract_text()
+            # Assuming the summary is located between the words "Summary:" and "Scope:"
+            start = text.find("Summary:") + len("Summary:")
+            end = text.find("Scope:")
+            if start != -1 and end != -1 and start < end:
+                return text[start:end].strip()
+            else:
+                return "Summary not found"
+    except requests.HTTPError as http_err:
+        return f"HTTP error occurred: {http_err}"
+    except Exception as err:
+        return f"An error occurred: {err}"
+
+# Extract summaries from the PDFs
+df_updated['Summary'] = df_updated['Title Name'].apply(extract_summary_from_url)
+
+# Save the updated DataFrame to an Excel file
+updated_excel_filename = '/mnt/data/business_documents_updated.xlsx'
+df_updated.to_excel(updated_excel_filename, index=False)
+
+updated_excel_filename, df_updated
